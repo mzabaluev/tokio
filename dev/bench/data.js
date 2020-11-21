4f85f9cb8a227cc43b1561fcc047d1e63df5f8ea
github-action-benchmark <github@users.noreply.github.com>
2020-11-21 22:38:54 +0000

    add sync_mpsc (cargo) benchmark result for f927f01a34d7cedf0cdc820f729a7a6cd56e83dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605998329300,
+  "lastUpdate": 1605998334231,
   "repoUrl": "https://github.com/mzabaluev/tokio",
   "entries": {
     "sync_rwlock": [
@@ -159,6 +159,92 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 21422965,
             "range": "± 2565494",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "sync_mpsc": [
+      {
+        "commit": {
+          "author": {
+            "email": "eliza@buoyant.io",
+            "name": "Eliza Weisman",
+            "username": "hawkw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "f927f01a34d7cedf0cdc820f729a7a6cd56e83dd",
+          "message": "macros: fix rustfmt on 1.48.0 (#3160)\n\n## Motivation\r\n\r\nLooks like the Rust 1.48.0 version of `rustfmt` changed some formatting\r\nrules (fixed some bugs?), and some of the code in `tokio-macros` is no\r\nlonger correctly formatted. This is breaking CI.\r\n\r\n## Solution\r\n\r\nThis commit runs rustfmt on Rust 1.48.0. This fixes CI.\r\n\r\nCloses #3158",
+          "timestamp": "2020-11-20T10:19:26-08:00",
+          "tree_id": "bd0243a653ee49cfc50bf61b00a36cc0fce6a414",
+          "url": "https://github.com/mzabaluev/tokio/commit/f927f01a34d7cedf0cdc820f729a7a6cd56e83dd"
+        },
+        "date": 1605998333337,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 6806373,
+            "range": "± 2474522",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6826577,
+            "range": "± 1566735",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 6040723,
+            "range": "± 2017668",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 737,
+            "range": "± 79",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 746,
+            "range": "± 69",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 759,
+            "range": "± 131",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 50861,
+            "range": "± 7367",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 780,
+            "range": "± 80",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1063670,
+            "range": "± 120330",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 779647,
+            "range": "± 102663",
             "unit": "ns/iter"
           }
         ]
